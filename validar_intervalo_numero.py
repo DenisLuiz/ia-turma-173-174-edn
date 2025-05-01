65fa492ff8b92bd4b4ff185f4e57c1cdce11a3db
Denis Luiz dos Santos <denisluiz91@yahoo.com>
2025-05-01 14:36:18 -0300

    adicionando script validar_intervalo_numero.py

---
+++ validar_intervalo_numero.py
@@ -0,0 +1,8 @@
+#Quero saber se um número está entre 10 e 20
+
+número = int(input("Digite seu numero: "))
+
+if número > 10 and número <= 20:
+    print(" O numero esta entre 10 e 20")
+else:
+    print("O Numero esta fora do intervalo")
