645e1e66ed45079ff9bb3447fe72987f4ca5fdf4
Denis Luiz dos Santos <denisluiz91@yahoo.com>
2025-05-01 13:37:00 -0300

    adicionando o script calculadora_imc.py

---
+++ calculadora_imc.py
@@ -0,0 +1,18 @@
+#calculadora de IMC
+#PESO DIVIDIDO PEA ALTURA **2
+
+altura = float(input("digite a sua altura em metros:"))
+peso = float(input("Digite o seu peso em kg :"))
+
+imc = peso /(altura ** 2)
+
+print(f" sei IMC é {imc:.2f}")
+
+if imc < 18.5:
+    print("Abaixo do peso")
+elif imc < 25:
+    print("Peso normal")
+elif imc < 30:
+    print("Sobrepeso")
+else:
+    print("Acima do peso")
